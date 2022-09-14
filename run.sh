20cb94ba50b204ebffa37209be93d003c46350b1
CipherX <102590998+CipherX-B4ar3@users.noreply.github.com>
2022-09-14 03:24:57 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-apt install python3.9
-pkg install python3.9
 apt update
 python -m pip install --upgrade pip
 pip install aiohttp
@@ -13,4 +11,4 @@ pip install selfCipherX
 pip install pycryptodome
 apt update
 clear
-python bot.py
+python CipherX.py
